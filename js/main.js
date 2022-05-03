cf5b3341213b32f73b76c1768573db8a17ca338c
Igor Boitsev <igor.boitsev@gmail.com>
2022-05-03 22:00:32 +0300

    simple forms

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import './modules/slider.js';
 import { modals } from "./modules/modals.js";
 import { tabs } from "./modules/tabs.js";
--- js/modules/forms.js
+++ js/modules/forms.js
@@ -1,12 +1,63 @@
 export const forms = () => {
 
-  const form = document.querySelectorAll('form'),
-        input = document.querySelectorAll('input');
+  const forms = document.querySelectorAll('form'),
+        inputs = document.querySelectorAll('input'),
+        phoneInputs = document.querySelectorAll('input[name="user_phone"]')
 
   const message = {
     loading: 'Звгрузка...',
     success: 'Спасибо! Скоро мы с вами свяжемся!',
     failure: 'Что-то пошло не так...'
   };
+
+  const postData = async (url, form) => {
+    document.querySelector('.status').textContent = message.loading;
+    return await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        user_name: form.querySelector('[name="user_name"]'),
+        user_phone: form.querySelector('[name="user_phone"]')
+      })
+    });
+
+  };
+
+  const clearInputs = () => {
+    inputs.forEach(input => {
+      input.value = '';
+    })
+  };
+
+  phoneInputs.forEach(phoneInput => {
+    phoneInput.addEventListener('input', () => {
+      phoneInput.value = phoneInput.value.replace(/\D/, '');
+    })
+  })
+
+  forms.forEach(form => {
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      
+      let statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      form.appendChild(statusMessage);
+
+      // const formData = new FormData(item);
+
+      postData('https://jsonplaceholder.typicode.com/posts', form)
+        .then(response => {
+          console.log(response.ok);
+          console.log(response.status);
+          statusMessage.textContent = message.success;
+        })
+        .catch(error => statusMessage.textContent = message.failure)
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 3000);
+        })
+    })
+  });
 
 }
